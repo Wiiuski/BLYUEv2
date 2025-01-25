65879a6e309d338a1e3d6587bfc6f38cf6cfea14
Wiiuski <cdthebandicoot9@gmail.com>
2025-01-25 12:42:16 -0500

    New repo

---
+++ Assets/Script/CameraCollide.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCollide : MonoBehaviour
+{
+    public float minDistance;
+    public float maxDistance;
+    public float smooth = 10;
+    Vector3 dollyDir;
+    public Vector3 dollyDirAdj;
+    public float distance;
+    public LayerMask objectLayers;
+
+    public bool woke;
+
+    
+
+    public void Start()
+    {
+        dollyDir = transform.localPosition.normalized;
+        distance = transform.localPosition.magnitude;
+
+        woke = true;
+    }
+   
+    void Update()
+    {
+        if (woke)
+        {
+            Vector3 cameraPOSDesired = transform.parent.TransformPoint(dollyDir * maxDistance);
+            RaycastHit hit;
+            if (Physics.Linecast(transform.parent.position, cameraPOSDesired, out hit, objectLayers))
+            {
+                distance = Mathf.Clamp(hit.distance * 0.8f, minDistance, maxDistance);
+            }
+            else
+            {
+                distance = maxDistance;
+            }
+            transform.localPosition = Vector3.Lerp(transform.localPosition, dollyDir * distance, Time.deltaTime * smooth);
+        }
+        
+    }
+}
---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float CameraSpeed = 100f;
+    public GameObject FollowObj;
+    Vector3 FollowPOS;
+    public int mouseSensitivity;
+    public GameObject CameraObj;
+    public GameObject PlayerObj;
+    public float camDistanceXToPlayer;
+    public float camDistanceYToPlayer;
+    public float camDistanceZToPlayer;
+    public float mouseX;
+    public float mouseY;
+    public float smoothX;
+    public float smoothY;
+    public float rotY;
+    public float rotX;
+    public bool cameraLocked = true;
+    public Quaternion localRotation;
+    public Vector3 localPos;
+    public Vector3 initPos;
+    public Quaternion initRot;
+    public Vector3 cameraPos;
+    public Quaternion cameraRot;
+
+    public bool woke;
+
+    //public GameMain main;
+
+    static float ClampAngle(float angle, float min, float max)
+    {
+        if (min < 0 && max > 0 && (angle > max || angle < min))
+        {
+            angle -= 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+        else if (min > 0 && (angle > max || angle < min))
+        {
+            angle += 360;
+            if (angle > max || angle < min)
+            {
+                if (Mathf.Abs(Mathf.DeltaAngle(angle, min)) < Mathf.Abs(Mathf.DeltaAngle(angle, max))) return min;
+                else return max;
+            }
+        }
+
+        if (angle < min) return min;
+        else if (angle > max) return max;
+        else return angle;
+    }
+
+
+    public void Start()
+    {
+
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        woke = true;
+    }
+
+    
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (woke)
+        {
+                rotX += -Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+                rotY += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+                rotX = ClampAngle(rotX, -60, 60);
+                rotY = ClampAngle(rotY, -180, 180);
+                localRotation = Quaternion.Euler(rotX, rotY, 0f);
+                transform.rotation = localRotation;
+        }
+    }
+    void LateUpdate()
+    {
+        if (woke)
+        {
+                CameraUpdater();
+                
+                        cameraPos = transform.position;
+                        localPos = cameraPos;
+                        cameraRot = transform.rotation;
+                    
+            
+        }
+    }
+    void CameraUpdater()
+    {
+        Transform target = FollowObj.transform;
+        float step = CameraSpeed * Time.fixedDeltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+
+    public void SetCamPos()
+    {
+            transform.position = cameraPos;
+            transform.rotation = cameraRot;
+            localRotation = cameraRot;
+    }
+
+
+
+}
